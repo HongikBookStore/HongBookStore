8443e97b856d0c6040a062440de97471a2a6f1a1
강유 <leek0729@naver.com>
2025-08-25 19:44:35 +0900

    챗봇 임포트 경로 정정 및 키 대응

--- src/main/frontend/src/components/ui/FloatingChatBot.jsx
+++ src/main/frontend/src/components/ui/FloatingChatBot.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled, { keyframes } from 'styled-components';
-import ChatBotContent from '../ChatBotModal/ChatBotModal';
+import ChatBotContent from '../ChatBotModal/ChatBotContent';
 import { useTranslation } from 'react-i18next';
 
 const fadeIn = keyframes`
