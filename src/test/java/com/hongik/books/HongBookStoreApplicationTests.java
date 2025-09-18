254d6d672b0f71af2c12f6aaa6f7b8f7623eb927
Mingyu Joung <samu9nai@gmail.com>
2025-09-18 19:53:48 +0900

    github action 수정4

--- src/test/java/com/hongik/books/HongBookStoreApplicationTests.java
+++ src/test/java/com/hongik/books/HongBookStoreApplicationTests.java
@@ -3,10 +3,16 @@ package com.hongik.books;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.google.cloud.storage.Storage;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class HongBookStoreApplicationTests {
+
+    @MockBean
+    Storage storage;
 
     @Test
     void contextLoads() {
